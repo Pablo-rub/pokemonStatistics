ded63b4c624df446ec9e0986c86244e85754fc43
p <pabloruar03@uma.es>
2025-04-18 20:44:55 +0200

    basic schema analyze battle

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,27 @@
-import { Route, Routes } from "react-router-dom";
-
-import HomePage from "./Pages/HomePage.jsx";
-import TurnAssistantPage from "./Pages/TurnAssistantPage.jsx";
-import RankingsPage from "./Pages/RankingsPage.jsx";
-import MainLayout from "./layout/MainLayout.jsx";
-import PublicGamesPage from "./Pages/PublicGamesPage.jsx";
-import ProfilePage from "./Pages/ProfilePage.jsx";
-import FinishSignIn from "./Pages/FinishSignIn.jsx";
-import SavedGamesPage from "./Pages/SavedGamesPage.jsx";
-import ForumPage from "./Pages/ForumPage.jsx";
-import ForumTopicPage from "./Pages/ForumTopicPage.jsx";
-import ContactPage from './Pages/ContactPage';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import MainLayout from './layout/MainLayout';
+import HomePage from './Pages/HomePage';
+import SavedGamesPage from './Pages/SavedGamesPage';
+import PublicGamesPage from './Pages/PublicGamesPage';
+import RankingsPage from './Pages/RankingsPage';
+import TurnAssistantPage from './Pages/TurnAssistantPage';
+import ForumPage from './Pages/ForumPage';
+import ProfilePage from './Pages/ProfilePage';
+import AnalyzeBattlePage from './Pages/AnalyzeBattlePage';
 
 function App() {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/turn-assistant" element={<TurnAssistantPage />} />
-        <Route path="/saved-games" element={<SavedGamesPage />} />
-        <Route path="/public-games" element={<PublicGamesPage />} />
-        <Route path="/rankings" element={<RankingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/finishSignIn" element={<FinishSignIn />} />
-        <Route path="/forum" element={<ForumPage />} />
-        <Route path="/forum/:topicId" element={<ForumTopicPage />} />
-        <Route path="/contact" element={<ContactPage />} />
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="saved-games" element={<SavedGamesPage />} />
+        <Route path="public-games" element={<PublicGamesPage />} />
+        <Route path="rankings" element={<RankingsPage />} />
+        <Route path="turn-assistant" element={<TurnAssistantPage />} />
+        <Route path="forum" element={<ForumPage />} />
+        <Route path="profile" element={<ProfilePage />} />
+        <Route path="analyze-battle/:replayId" element={<AnalyzeBattlePage />} />
       </Route>
     </Routes>
   );
