8eca1442ab962def95acad64b2a05ee1b8815c22
Pablo-Rub <pabloruar03@uma.es>
2025-08-12 17:49:28 +0200

    .

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use(express.json());
 
 // Initialize the BigQuery client with explicit credentials
 const bigQuery = new BigQuery({
-  keyFilename: "D:/tfg/pokemonStatistics/credentials.json",
+  //keyFilename: "D:/tfg/pokemonStatistics/credentials.json",
 });
 
 // Rutas para robots.txt y sitemap.xml
