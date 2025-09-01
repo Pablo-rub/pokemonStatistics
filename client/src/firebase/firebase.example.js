da252dae34a1a0373bc70540a886b85d426bbd02
Pablo-Rub <pabloruar03@uma.es>
2025-09-01 18:26:18 +0200

    firebase con variables locales

---
+++ client/src/firebase/firebase.example.js
@@ -0,0 +1,12 @@
+// Copia este archivo a client/src/firebase/firebase.js y completa con tus valores locales.
+// NO subir client/src/firebase/firebase.js al repositorio.
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  apiKey: "<YOUR_API_KEY>",
+  authDomain: "<PROJECT>.firebaseapp.com",
+  projectId: "<PROJECT>",
+  storageBucket: "<PROJECT>.appspot.com",
+  messagingSenderId: "<SENDER_ID>",
+  appId: "<APP_ID>",
+  measurementId: "<MEASUREMENT_ID>"
+};
